81a78edc9b3d699fbc0329c0cc874c7c307287df
FellypeGC <gabrielfellypesteve@gmail.com>
2025-05-09 15:50:05 -0300

    english translations almost done

---
+++ translations.js
@@ -0,0 +1,24 @@
+const translations = {
+  en: {
+    aboutLink: "About",
+    skillsLink: "Skills",
+    projectsLink: "Projects",
+    contactLink: "Contact",
+    homeTitle: "Front-End Developer",
+    aboutTitle: "About Me",
+    skillsTitle: "My skills",
+    projectsTitle: "Projects",
+    homeP1: "Hello, World! I'm Fellype.",
+    homeP2: "As a Front-End Developer, I focus on learning and building software for Web Systems with responsive, intuitive, visually appealing user interfaces.",
+    aboutP1: "Currently in Front-End applications development, planning to become a Full-Stack with a international career creating high-performance and efficient softwares for each platform.",
+    aboutP2: "Highly interested in expanding my knowledge and working across a range of technologies including Desktop, Front-End, Back-End, Mobile and even Games, to solve complex problems continuously improving my skills.",
+    aboutP3: "I'm either at Technical Program in Systems Analysis and Development.",
+    skillsP: "Technologies that I know and have also used",
+    description1: "",
+    description2: "",
+    description3: "",
+  },
+  ptBr: {
+
+  }
+}
