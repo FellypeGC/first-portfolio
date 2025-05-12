9b1af9d292ae4f5571bc941a68ebb0fed001cb11
FellypeGC <gabrielfellypesteve@gmail.com>
2025-05-12 12:09:45 -0300

    media queries atualizadas e header responsivo

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Header Navigation 
+const toggleNavButton = document.querySelector(".bi-list");
+const navBar = document.querySelector("ul");
+
+toggleNavButton.addEventListener("click", () => {
+  toggleNavButton.classList.toggle("bi-x-lg");
+  navBar.classList.toggle("active");
+});
+
+
 // Footer 
 const footerP = document.querySelector('.footer-p');
 let year = new Date().getFullYear();
