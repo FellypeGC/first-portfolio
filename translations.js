538d8533d6468d73dcb2525874e537838520b198
FellypeGC <gabrielfellypesteve@gmail.com>
2025-05-15 14:34:32 -0300

    alteração

--- translations.js
+++ translations.js
@@ -15,7 +15,7 @@ const translations = {
     aboutP2: "Highly interested in expanding my knowledge and working across a range of technologies including Desktop, Front-End, Back-End, Mobile and even Games, to solve complex problems continuously improving my skills.",
     aboutP3: "I'm also enrolled in a Technical Program in Systems Analysis and Development.",
     skillsP: "Technologies that I know and have also used",
-    description1: "Landing page developed as a clone of the Reserva brand website, using HTML, CSS/SASS, and JavaScript. The project simulates sections such as product showcases, categories, and promotional highlights. Designed to replicate the visual and interactive experience of a men's fashion e-commerce site.",
+    description1: "Landing page developed as a clone of the Reserva brand website, using HTML, CSS/SASS, and JavaScript. The project simulates sections such as product showcases, categories, and promotional highlights. Designed to replicate the visual and interactive experience of a fashion e-commerce site.",
     description2: "Responsive landing page for the Overmatch English course (Now, it's Phenom Idiomas), using HTML, CSS, and JavaScript. Includes sections such as registration form, testimonials, and location. Designed to promote private online and in-person classes in an accessible and modern way.",
     description3: "Landing page developed for the fictional pizzeria PizzaHash, using HTML, CSS, and JavaScript. The site features sections such as menu, promotions, and reviews. Designed to simulate a real gastronomic e-commerce experience.",
   },
@@ -35,7 +35,7 @@ const translations = {
     aboutP2: "Tenho grande interesse em expandir meus conhecimentos e atuar em diversas tecnologias, incluindo Desktop, Front-End, Back-End, Dispositivos Móveis e até Jogos, buscando solucionar problemas complexos e aprimorar continuamente minhas habilidades.",
     aboutP3: "Além disso, estou cursando Análise e Desenvolvimento de Sistemas.",
     skillsP: "Tecnologias que eu tenho conhecimento e já usei",
-    description1: "Landing page desenvolvida como clone do site da marca Reserva, utilizando HTML, CSS/SASS e JavaScript. O projeto simula seções como vitrines de produtos, categorias e destaques promocionais. Criado para reproduzir a experiência visual e interativa de um e-commerce de moda masculina.",
+    description1: "Landing page desenvolvida como clone do site da marca Reserva, utilizando HTML, CSS/SASS e JavaScript. O projeto simula seções como vitrines de produtos, categorias e destaques promocionais. Criado para reproduzir a experiência visual e interativa de um e-commerce de moda.",
     description2: "Landing page responsiva para o curso de inglês Overmatch (Agora, Phenom Idiomas), utilizando HTML, CSS e JavaScript. Inclui seções como formulário de inscrição, depoimentos e localização. Projetada para promover aulas particulares online e presenciais de forma acessível e moderna.",
     description3: "Landing page desenvolvida para a pizzaria fictícia PizzaHash, utilizando HTML, CSS e JavaScript. O site apresenta seções como menu, promoções e avaliações. Projetado para simular uma experiência real de e-commerce gastronômico.",
   }
