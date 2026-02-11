bc31b8eec8adb9c1e35eb54f727f35fdb98246f9
FellypeGC <gabrielfellypesteve@gmail.com>
2026-02-11 17:11:49 -0300

    chore: StatusConfig naming changed

--- src/components/StatusBadge.tsx
+++ src/components/StatusBadge.tsx
@@ -3,7 +3,7 @@ type StatusBadgeProps = {
   companyName: string;
 };
 
-type StatusClasses = {
+type StatusConfig = {
   statusClasses: string;
   dotClasses: string;
   label: string;
@@ -11,7 +11,7 @@ type StatusClasses = {
 
 const StatusBadge = ({ isAvailable, companyName }: StatusBadgeProps) => {
   // 1. Definimos as variáveis de estilo baseadas no status
-  const status: StatusClasses = {
+  const status: StatusConfig = {
     statusClasses: isAvailable ? "bg-green-500/10 text-green-500 border-green-500/20" : "bg-[rgba(37,106,244,0.1)] text-[rgb(37,106,244)] border-[rgba(37,106,244,0.2)]",
     dotClasses: isAvailable ? "bg-green-500" : "bg-[rgb(37,106,244)]",
     label: isAvailable ? "Available for Hire" : `Working at ${companyName}`,
