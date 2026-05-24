8cab854ab4566f82da2992e9d9567efab8df5323
FellypeGC <gabrielfellypesteve@gmail.com>
2026-05-24 13:40:00 -0300

    remove logo bg blue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="bg-color-[#0A0B10]">
       <div className="flex shadow-md mx-auto p-4 items-center justify-around">
         <div className="flex gap-2.5 items-center">
-          <div className="bg-blue-700 rounded-[7px] flex justify-center items-center p-1">
+          <div className="rounded-[7px] flex justify-center items-center p-1"> {/** bg-blue-700 */}
             <img src={logo} alt="Code Icon Logo" className="h-7 w-8" />
           </div>
           <h1 className="text-2xl font-semibold">fllpdev.ts</h1>
