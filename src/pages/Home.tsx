0233783cabb67c57df2b2bf7c69a27e3a5200371
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-05 23:53:40 -0300

    feat: add Link component to projects and contact page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 // import Button from '../components/Button'
-import Button from '../components/Button';
+import { Link } from 'react-router';
 import StatusBadge from '../components/StatusBadge';
 import TerminalCard from '../components/TerminalCard';
+import { ArrowRight } from "lucide-react"
 
 const Home = () => {
   const isAvailable: boolean = true;
@@ -19,13 +20,22 @@ const Home = () => {
           Hello, World! I'm a Software Developer focused on Front-End rather than Back-End, building reliable, scalable and custom-made applications.
         </p>
 
-        <Button 
-          className="w-40 h-16 text-white cursor-pointer text-base font-bold text-center bg-[rgb(37,106,244)] rounded-[10px] outline-none border-none transition-all duration-200 ease-in-out delay-0 hover:opacity-90 hover:scale-[0.95]" text="View Projects" 
-        />
-        
-        <Button 
-          className="w-40 h-16 text-white cursor-pointer text-base font-bold text-center bg-[#0c0c0c] rounded-[10px] outline-none border border-amber-50 transition-all duration-200 ease-in-out delay-0 hover:opacity-90 hover:scale-[0.95]" text="View Projects" 
-        />
+        <div className="flex gap-4">
+          <Link 
+            to="/projects" 
+            className="flex items-center justify-center w-40 h-14 text-white cursor-pointer text-base font-bold text-center bg-[rgb(37,106,244)] rounded-[10px] outline-none border-none transition-all duration-200 ease-in-out delay-0 hover:opacity-90 hover:scale-[0.95]"
+          >
+            View Projects
+            <ArrowRight className="ml-2" />
+          </Link>
+          
+          <Link
+            to="/contact"
+            className="flex items-center justify-center w-40 h-14 text-white cursor-pointer text-base font-bold text-center bg-[#0c0c0c] rounded-[10px] outline-none border border-white/10 transition-all duration-200 ease-in-out delay-0 hover:opacity-90 hover:scale-[0.95]"
+          >
+            Contact Me
+          </Link>
+        </div>
       </div>
 
       {/* VS Code Terminal Card */}
