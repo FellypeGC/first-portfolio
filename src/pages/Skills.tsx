475b18f339d5810f390407901e9ca73b08170639
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-08 23:48:48 -0300

    chore: remove padding and max-w-full from skills section

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,6 +1,6 @@
 const Skills = () => {
   return (
-    <section className="text-white flex justify-center items-center gap-8 px-6 md:py-24 md:px-20 max-w-full w-300 mx-auto flex-wrap">
+    <section className="text-white flex justify-between items-center gap-8 max-w-300 mx-auto flex-wrap">
       <div className="flex w-96 h-100 bg-red-600">Technical Expertise</div>
       <div className="flex w-3xl h-100 bg-white text-black">Card boxes</div>
     </section>
