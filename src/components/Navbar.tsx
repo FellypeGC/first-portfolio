18577fedaabdfd03a3837ae87120908da2f3403a
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-24 19:24:11 -0300

    fix(portfolio): vercel SPA fallback + habit-track image path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
             </ul>
             <LanguageToggle />
             <a
-              href={`/public/${resumeUrl}`}
+              href={resumeUrl}
               download={resumeFileName}
               className="flex items-center gap-1 px-4 py-2 bg-blue-700 cursor-pointer text-sm font-bold rounded-[7px] transition-all duration-150 hover:opacity-90 hover:shadow-[0_0_15px_rgba(37,106,244,0.6)] hover:border-[rgba(37,106,244,0.8)] hover:scale-[0.98]"
             >
@@ -80,7 +80,7 @@ const Navbar = () => {
           <LanguageToggle />
           <div>
             <a
-              href={`/public/${resumeUrl}`}
+              href={resumeUrl}
               download={resumeFileName}
               className="w-full justify-center flex items-center gap-1 px-4 py-3 bg-blue-700 cursor-pointer text-sm font-bold rounded-[7px] transition-all duration-150 hover:opacity-90"
             >
